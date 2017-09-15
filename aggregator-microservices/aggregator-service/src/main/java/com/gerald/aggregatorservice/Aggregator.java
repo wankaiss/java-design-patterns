71a817c67cc5fde1b95f7142b4e7a52926896caa
yangqianhui <yangqianhui@youbanban.com>
2017-09-15 11:48:55 +0800

    aggregator-microservices pattern init

---
+++ aggregator-microservices/aggregator-service/src/main/java/com/gerald/aggregatorservice/Aggregator.java
@@ -0,0 +1,7 @@
+package com.gerald.aggregatorservice;
+
+/**
+ * Created by Gerald_Yang on 2017/9/15.
+ */
+public class Aggregator {
+}
---
+++ aggregator-microservices/inventory-microservice/src/main/java/com/gerald/inventorymicroservice/InventoryController.java
@@ -0,0 +1,7 @@
+package com.gerald.inventorymicroservice;
+
+/**
+ * Created by Gerald_Yang on 2017/9/15.
+ */
+public class InventoryController {
+}
